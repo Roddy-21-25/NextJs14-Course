d4f050407c2463cce822b7498a22527df0f4564c
Roddy-Rafael-Tejeda-Rosario-21-25 <128174391+Roddy-21-25@users.noreply.github.com>
2024-01-06 13:46:41 -0400

    first commit

---
+++ src/app/not-found.jsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+const NotFound = () => {
+    return <div>
+        <h2>Not Found Page</h2>
+        <Link href='/'>Return Home</Link>
+    </div>;
+};
+
+export default NotFound;
